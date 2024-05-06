466f14f340b82c5afe12c59c45cf06e154a85dd3
Adriana Bamberger <162661198+Adriana-Bamberger@users.noreply.github.com>
2024-05-07 10:05:34 +1200

    fun with numbers done

--- client/components/FunWithNumbers.tsx
+++ client/components/FunWithNumbers.tsx
@@ -2,17 +2,41 @@ import { useState } from 'react'
 
 function FunWithNumbers() {
   // TODO: create state
-
+  const [currentNum, setCurrentNum] = useState(5)
   return (
     <>
       <h2>Fun with Numbers</h2>
-      <p>{/*state goes in here*/}</p>
+      <p>{currentNum}</p>
 
       {/* TODO: create onClick code to update the state for each button */}
-      <button onClick={() => {}}>{/* something */}</button>
-      <button onClick={() => {}}>{/* something */}</button>
-      <button onClick={() => {}}>{/* something */}</button>
-      <button onClick={() => {}}>{/* something */}</button>
+      <button
+        onClick={() => {
+          setCurrentNum(currentNum + 5)
+        }}
+      >
+        +5
+      </button>
+      <button
+        onClick={() => {
+          setCurrentNum(currentNum * 5)
+        }}
+      >
+        x5
+      </button>
+      <button
+        onClick={() => {
+          setCurrentNum(currentNum - 5)
+        }}
+      >
+        -5
+      </button>
+      <button
+        onClick={() => {
+          setCurrentNum(currentNum * 0)
+        }}
+      >
+        Clear
+      </button>
     </>
   )
 }
